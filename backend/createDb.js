d4d262350e523a474ac582c98c7af095f8196e87
kramarmm <kramar.mariya@mail.ru>
2017-04-18 23:15:05 +0300

    added mongoose

--- backend/createDb.js
+++ backend/createDb.js
@@ -1,27 +1,20 @@
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/game');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/game';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
 
-  var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the document collection");
-    callback(result);
-  });
-}
 
-  db.close();
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({
+  name: 'Zildjian'
+});
+
+console.log(kitty);
+
+kitty.save(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('meow');
+  }
 });
--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(favicon(path.join(__dirname, '../public', 'favicon.ico'))); 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
-
+var db = require("./createDb.js");
 
 app.post('/login', function(req, res) {
     // usersArray.push(JSON.stringify(req.body));    
@@ -30,7 +30,7 @@ app.post('/login', function(req, res) {
     //     if (err) return console.log(err);
     // });
 
-    ( login.checkUserAuth(req.body.login, req.body.password) ) ? res.sendFile(path.join(__dirname, '../public/game.html')) : res.sendStatus(403);
+    ( login.checkUserAuth(req.body.login, req.body.password) ) ? fs.readdir(path.join(__dirname, '../public'), (err) => console.log(err)) : res.sendStatus(403);
     
     // return usersArray;
 

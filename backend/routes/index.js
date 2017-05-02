7461718ccbb9f75ac140e0da6f241a09d37e8b85
kramarmm <kramar.mariya@mail.ru>
2017-05-02 12:29:54 +0300

    adds checkAuth to "/comments" route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,9 +11,9 @@ module.exports = (app) => {
     app.post("/game", require("./game").post);
     app.put("/game", require("./game").put);
 
-    app.get("/game/boss", require("./game_boss").get);
+    app.get("/game/boss",checkAuth, require("./game_boss").get);
 
-    app.get("/comments", require("./comments").get);
+    app.get("/comments", checkAuth, require("./comments").get);
     app.post("/comments", require("./comments").post);
 
     app.get("/comments/all", require("./comments_all").get);
